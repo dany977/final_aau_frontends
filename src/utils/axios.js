afb12f14bd031cba481a7ada61ab38457442ebbc
daniel alemu <danie@DESKTOP-AKI1CR5.moa.gov.et>
2025-12-08 13:50:12 +0300

    Fix CORS origins

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7000/api",
+  baseURL: "https://final-project-aau-backend.onrender.com",
  withCredentials: true,
 });
 
