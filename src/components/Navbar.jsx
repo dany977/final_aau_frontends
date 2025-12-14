761f9e5cc435dfec3c6cf36e51eef09a8791fa65
daniel <danielalemu33@gmail.com>
2025-12-14 17:12:57 +0300

    Fix React is not defined error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
