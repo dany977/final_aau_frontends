9704949804893625b4363a722532d5018ff0d6b5
daniel alemu <danie@DESKTOP-AKI1CR5.moa.gov.et>
2025-12-17 09:04:49 +0300

    Add dashboard stats API

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://final-project-aau-backend.onrender.com/api",
+  baseURL: "https://final-project-aau-backend.onrender.com",
 });
 
-// 🔐 ADD TOKEN TO EVERY REQUEST
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token"); // 👈 must exist
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default instance;
